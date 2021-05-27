177f8ad9fef02c072c6058a3cc4e15a1d10742f3
shinyeeun789 <tlsdpdms789@naver.com>
2021-05-27 18:56:50 +0900

    화면 수정

--- app/src/main/java/com/inhatc/mobile_project/recyclerItem.java
+++ app/src/main/java/com/inhatc/mobile_project/recyclerItem.java
@@ -3,6 +3,6 @@ package com.inhatc.mobile_project;
 import android.widget.ImageView;
 
 public class recyclerItem {
-    private String userImage;
+    private String userImage;       // 이미지 어떻게 가져올지??
 
 }
